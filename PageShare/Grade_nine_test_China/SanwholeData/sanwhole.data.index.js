3d8c78de7acf4a7c34ec075fcce5e2975ffbd25a
Sanwhole <49502331+sanwhole@users.noreply.github.com>
2025-09-04 10:17:46 +0800

    Add files via upload

---
+++ PageShare/Grade_nine_test_China/SanwholeData/sanwhole.data.index.js
@@ -0,0 +1,10 @@
+SAN_WHOLE.namespace( 'SAN_WHOLE.data_index' );
+SAN_WHOLE.data_index = {
+		DREX_NODE_KEYWORDS: [],
+	DREX_NODE_KEYWORDS_START: [0,0,0,0,0], //length:= drex.nodes_count,
+	DREX_NODE_KEYWORDS_END: [0,0,0,0,0], //length:= drex.nodes_count,
+	DREX_KEYWORD_NAMES: ["<NEW KEYWORD>"],
+	DREX_KEYWORD_CHILD_START: [1],
+	DREX_KEYWORD_CHILD_END: [1]
+	
+};
